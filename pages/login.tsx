3fb3bfd1d2372a904b0239d35b59d4169bdefa7e
Grayson Crozier <gcrozier@rsrgroup.com>
2024-05-06 09:50:07 -0400

    Update login/signup

--- pages/login.tsx
+++ pages/login.tsx
@@ -6,6 +6,7 @@ import { PageTransition } from 'components/motion/page-transition'
 import { Section } from 'components/section'
 import { NextPage } from 'next'
 import { FaGithub, FaGoogle } from 'react-icons/fa'
+import { SEO } from "components/seo/seo";
 
 const providers = {
   google: {
@@ -21,19 +22,25 @@ const providers = {
 
 const Login: NextPage = () => {
   return (
-    <Section height="calc(100vh - 200px)" innerWidth="container.sm">
-      <BackgroundGradient zIndex="-1" />
+    <>
+      <SEO
+        title="Volare Solutions - IT Services"
+        description="Providing comprehensive digital solutions including portfolio websites, e-commerce platforms, and software contract work."
+      />
+      <Section height="calc(100vh - 200px)" innerWidth="container.sm">
+        <BackgroundGradient zIndex="-1" />
 
-      <Center height="100%" pt="20">
-        <PageTransition width="100%">
-          <Auth
-            view="login"
-            providers={providers}
-            signupLink={<Link href="/signup">Sign up</Link>}
-          />
-        </PageTransition>
-      </Center>
-    </Section>
+        <Center height="100%" pt="20">
+          <PageTransition width="100%">
+            <Auth
+              view="login"
+              providers={providers}
+              signupLink={<Link href="/signup">Sign up</Link>}
+            />
+          </PageTransition>
+        </Center>
+      </Section>
+    </>
   )
 }
 
