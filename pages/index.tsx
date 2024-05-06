b2cd2d5e1edcec595624b035daca72850ff868d6
Grayson Crozier <gcrozier@rsrgroup.com>
2024-05-06 09:49:54 -0400

    Update home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,8 +35,8 @@ import {
   FiGrid,
   FiLock,
   FiSearch,
-  FiSliders,
-  FiSmile,
+  FiShoppingCart,
+  FiMonitor,
   FiTerminal,
   FiThumbsUp,
   FiToggleLeft,
@@ -65,15 +65,15 @@ const Home: NextPage = () => {
   return (
     <Box>
       <SEO
-        title="Saas UI Landingspage"
-        description="Free SaaS landingspage starter kit"
+        title="Volare Solutions - IT Services"
+        description="Providing comprehensive digital solutions including portfolio websites, e-commerce platforms, and software contract work."
       />
       <Box>
         <HeroSection />
 
         <HighlightsSection />
 
-        <FeaturesSection />
+        {/* <FeaturesSection /> */}
 
         <TestimonialsSection />
 
@@ -97,30 +97,28 @@ const HeroSection: React.FC = () => {
             px="0"
             title={
               <FallInPlace>
-                Build beautiful
-                <Br /> software faster
+                Powering Digital <br />
+                Transformation
               </FallInPlace>
             }
             description={
               <FallInPlace delay={0.4} fontWeight="medium">
-                Saas UI is a <Em>React component library</Em>
-                <Br /> that doesn&apos;t get in your way and helps you <Br />{" "}
-                build intuitive SaaS products with speed.
+                Specializing in creating portfolio websites, developing e-commerce solutions, and offering tailored software contract services to boost your business's digital presence and operational efficiency.
               </FallInPlace>
             }
           >
             <FallInPlace delay={0.8}>
-              <HStack pt="4" pb="12" spacing="8">
+              {/* <HStack pt="4" pb="12" spacing="8">
                 <NextjsLogo height="28px" /> <ChakraLogo height="20px" />
-              </HStack>
+              </HStack> */}
 
-              <ButtonGroup spacing={4} alignItems="center">
+              <ButtonGroup paddingTop={8} spacing={4} alignItems="center">
                 <ButtonLink colorScheme="primary" size="lg" href="/signup">
-                  Sign Up
+                  Get Started
                 </ButtonLink>
                 <ButtonLink
                   size="lg"
-                  href="https://demo.saas-ui.dev"
+                  href="#pricing"
                   variant="outline"
                   rightIcon={
                     <Icon
@@ -135,7 +133,7 @@ const HeroSection: React.FC = () => {
                     />
                   }
                 >
-                  View demo
+                  View Pricing
                 </ButtonLink>
               </ButtonGroup>
             </FallInPlace>
@@ -167,40 +165,37 @@ const HeroSection: React.FC = () => {
       </Container>
 
       <Features
-        id="benefits"
+        id="services"
         columns={[1, 2, 4]}
         iconSize={4}
         innerWidth="container.xl"
         pt="20"
         features={[
           {
-            title: "Accessible",
-            icon: FiSmile,
-            description: "All components strictly follow WAI-ARIA standards.",
+            title: "Portfolio Websites",
+            icon: FiMonitor,
+            description: "Professional portfolio websites designed and developed to showcase your work.",
             iconPosition: "left",
             delay: 0.6,
           },
           {
-            title: "Themable",
-            icon: FiSliders,
-            description:
-              "Fully customize all components to your brand with theme support and style props.",
+            title: "E-commerce Platforms",
+            icon: FiShoppingCart,
+            description: "Robust e-commerce platforms built to boost your online sales.",
             iconPosition: "left",
             delay: 0.8,
           },
           {
-            title: "Composable",
-            icon: FiGrid,
-            description:
-              "Compose components to fit your needs and mix them together to create new ones.",
+            title: "Software Contract Work",
+            icon: FiCode,
+            description: "Contract work provided for existing software applications.",
             iconPosition: "left",
             delay: 1,
           },
           {
-            title: "Productive",
+            title: "Productivity Enhancement",
             icon: FiThumbsUp,
-            description:
-              "Designed to reduce boilerplate and fully typed, build your product at speed.",
+            description: "Streamlined operations to enhance productivity.",
             iconPosition: "left",
             delay: 1.1,
           },
@@ -216,90 +211,60 @@ const HighlightsSection = () => {
 
   return (
     <Highlights>
-      <HighlightsItem colSpan={[1, null, 2]} title="Core components">
+      <HighlightsItem colSpan={[1, null, 2]} title="Our Mission">
         <VStack alignItems="flex-start" spacing="8">
           <Text color="muted" fontSize="xl">
-            Get started for free with <Em>30+ open source components</Em>.
-            Including authentication screens with Clerk, Supabase and Magic.
-            Fully functional forms with React Hook Form. Data tables with React
-            Table.
+            Our mission is simple: to enable businesses to focus on what truly matters—
+            their product and service—by providing <Em>comprehensive, streamlined digital solutions.</Em> We
+            believe in not just meeting the digital era's demands but turning them into your competitive advantage.
           </Text>
-
-          <Flex
-            rounded="full"
-            borderWidth="1px"
-            flexDirection="row"
-            alignItems="center"
-            py="1"
-            ps="8"
-            pe="2"
-            bg="primary.900"
-            _dark={{ bg: "gray.900" }}
-          >
-            <Box>
-              <Text color="yellow.400" display="inline">
-                yarn add
-              </Text>{" "}
-              <Text color="cyan.300" display="inline">
-                @saas-ui/react
-              </Text>
-            </Box>
-            <IconButton
-              icon={hasCopied ? <FiCheck /> : <FiCopy />}
-              aria-label="Copy install command"
-              onClick={onCopy}
-              variant="ghost"
-              ms="4"
-              isRound
-              color="white"
-            />
-          </Flex>
+          <ButtonLink colorScheme="primary" size="lg" href="#pricing">
+            Get Started
+          </ButtonLink>
         </VStack>
       </HighlightsItem>
-      <HighlightsItem title="Solid foundations">
+      <HighlightsItem title="Solid Foundations">
         <Text color="muted" fontSize="lg">
-          We don&apos;t like to re-invent the wheel, neither should you. We
-          selected the most productive and established tools in the scene and
-          build Saas UI on top of it.
+          We don&apos;t like to re-invent the wheel, neither should you. We offer a quick 
+          turnaround by leveraging proven templates that have worked for businesses time and again. 
         </Text>
       </HighlightsItem>
       <HighlightsTestimonialItem
-        name="Renata Alink"
+        name="Grayson Crozier"
         description="Founder"
-        avatar="/static/images/avatar.jpg"
+        avatar="/static/images/grayson.jpg"
         gradient={["pink.200", "purple.500"]}
       >
-        “Saas UI helped us set up a beautiful modern UI in no time. It saved us
-        hundreds of hours in development time and allowed us to focus on
-        business logic for our specific use-case from the start.”
+        "At Volare Solutions, our focus is on our clients. We provide efficient digital solutions that 
+        transform the challenges of the digital era into competitive advantages. This approach allows our 
+        clients to concentrate on what truly matters - their core business."
       </HighlightsTestimonialItem>
       <HighlightsItem
         colSpan={[1, null, 2]}
-        title="Start your next idea two steps ahead"
+        title="Take Your Business to the Next Level"
       >
         <Text color="muted" fontSize="lg">
-          We took care of all your basic frontend needs, so you can start
-          building functionality that makes your product unique.
+          At Volare Solutions, we take care of all your technical needs, so you can focus on what makes your business unique.
         </Text>
         <Wrap mt="8">
           {[
-            "authentication",
-            "navigation",
-            "crud",
-            "settings",
-            "multi-tenancy",
-            "layouts",
-            "billing",
-            "a11y testing",
-            "server-side rendering",
-            "documentation",
-            "onboarding",
-            "storybooks",
-            "theming",
-            "upselling",
-            "unit testing",
-            "feature flags",
-            "responsiveness",
+            "web development",
+            "monetization",
+            "cloud image hosting",
+            "e-commerce platforms",
+            "email services",
+            "customer relationship management",
+            "analytics",
+            "mobile app development",
+            "software contract work",
+            "productivity enhancement",
+            "IT consulting",
+            "project management",
+            "quality assurance",
+            "system integration",
+            "security services",
+            "technical support",
+            "data migration",
           ].map((value) => (
             <Tag
               key={value}
@@ -317,105 +282,105 @@ const HighlightsSection = () => {
   );
 };
 
-const FeaturesSection = () => {
-  return (
-    <Features
-      id="features"
-      title={
-        <Heading
-          lineHeight="short"
-          fontSize={["2xl", null, "4xl"]}
-          textAlign="left"
-          as="p"
-        >
-          Not your standard
-          <Br /> dashboard template.
-        </Heading>
-      }
-      description={
-        <>
-          Saas UI Pro includes everything you need to build modern frontends.
-          <Br />
-          Use it as a template for your next product or foundation for your
-          design system.
-        </>
-      }
-      align="left"
-      columns={[1, 2, 3]}
-      iconSize={4}
-      features={[
-        {
-          title: "Components.",
-          icon: FiBox,
-          description:
-            "All premium components are available on a private NPM registery, no more copy pasting and always up-to-date.",
-          variant: "inline",
-        },
-        {
-          title: "Starterkits.",
-          icon: FiLock,
-          description:
-            "Example apps in Next.JS, Electron. Including authentication, billing, example pages, everything you need to get started FAST.",
-          variant: "inline",
-        },
-        {
-          title: "Documentation.",
-          icon: FiSearch,
-          description:
-            "Extensively documented, including storybooks, best practices, use-cases and examples.",
-          variant: "inline",
-        },
-        {
-          title: "Onboarding.",
-          icon: FiUserPlus,
-          description:
-            "Add user onboarding flows, like tours, hints and inline documentation without breaking a sweat.",
-          variant: "inline",
-        },
-        {
-          title: "Feature flags.",
-          icon: FiFlag,
-          description:
-            "Implement feature toggles for your billing plans with easy to use hooks. Connect Flagsmith, or other remote config services once you're ready.",
-          variant: "inline",
-        },
-        {
-          title: "Upselling.",
-          icon: FiTrendingUp,
-          description:
-            "Components and hooks for upgrade flows designed to make upgrading inside your app frictionless.",
-          variant: "inline",
-        },
-        {
-          title: "Themes.",
-          icon: FiToggleLeft,
-          description:
-            "Includes multiple themes with darkmode support, always have the perfect starting point for your next project.",
-          variant: "inline",
-        },
-        {
-          title: "Generators.",
-          icon: FiTerminal,
-          description:
-            "Extend your design system while maintaininig code quality and consistency with built-in generators.",
-          variant: "inline",
-        },
-        {
-          title: "Monorepo.",
-          icon: FiCode,
-          description: (
-            <>
-              All code is available as packages in a high-performance{" "}
-              <Link href="https://turborepo.com">Turborepo</Link>, you have full
-              control to modify and adjust it to your workflow.
-            </>
-          ),
-          variant: "inline",
-        },
-      ]}
-    />
-  );
-};
+// const FeaturesSection = () => {
+//   return (
+//     <Features
+//       id="features"
+//       title={
+//         <Heading
+//           lineHeight="short"
+//           fontSize={["2xl", null, "4xl"]}
+//           textAlign="left"
+//           as="p"
+//         >
+//           Not your standard
+//           <Br /> dashboard template.
+//         </Heading>
+//       }
+//       description={
+//         <>
+//           Saas UI Pro includes everything you need to build modern frontends.
+//           <Br />
+//           Use it as a template for your next product or foundation for your
+//           design system.
+//         </>
+//       }
+//       align="left"
+//       columns={[1, 2, 3]}
+//       iconSize={4}
+//       features={[
+//         {
+//           title: "Components.",
+//           icon: FiBox,
+//           description:
+//             "All premium components are available on a private NPM registery, no more copy pasting and always up-to-date.",
+//           variant: "inline",
+//         },
+//         {
+//           title: "Starterkits.",
+//           icon: FiLock,
+//           description:
+//             "Example apps in Next.JS, Electron. Including authentication, billing, example pages, everything you need to get started FAST.",
+//           variant: "inline",
+//         },
+//         {
+//           title: "Documentation.",
+//           icon: FiSearch,
+//           description:
+//             "Extensively documented, including storybooks, best practices, use-cases and examples.",
+//           variant: "inline",
+//         },
+//         {
+//           title: "Onboarding.",
+//           icon: FiUserPlus,
+//           description:
+//             "Add user onboarding flows, like tours, hints and inline documentation without breaking a sweat.",
+//           variant: "inline",
+//         },
+//         {
+//           title: "Feature flags.",
+//           icon: FiFlag,
+//           description:
+//             "Implement feature toggles for your billing plans with easy to use hooks. Connect Flagsmith, or other remote config services once you're ready.",
+//           variant: "inline",
+//         },
+//         {
+//           title: "Upselling.",
+//           icon: FiTrendingUp,
+//           description:
+//             "Components and hooks for upgrade flows designed to make upgrading inside your app frictionless.",
+//           variant: "inline",
+//         },
+//         {
+//           title: "Themes.",
+//           icon: FiToggleLeft,
+//           description:
+//             "Includes multiple themes with darkmode support, always have the perfect starting point for your next project.",
+//           variant: "inline",
+//         },
+//         {
+//           title: "Generators.",
+//           icon: FiTerminal,
+//           description:
+//             "Extend your design system while maintaininig code quality and consistency with built-in generators.",
+//           variant: "inline",
+//         },
+//         {
+//           title: "Monorepo.",
+//           icon: FiCode,
+//           description: (
+//             <>
+//               All code is available as packages in a high-performance{" "}
+//               <Link href="https://turborepo.com">Turborepo</Link>, you have full
+//               control to modify and adjust it to your workflow.
+//             </>
+//           ),
+//           variant: "inline",
+//         },
+//       ]}
+//     />
+//   );
+// };
 
 const TestimonialsSection = () => {
   const columns = React.useMemo(() => {
@@ -471,7 +436,7 @@ export async function getStaticProps() {
         title: "Support us by becoming a stargazer! 🚀 ",
         description:
           '<img src="https://img.shields.io/github/stars/saas-js/saas-ui.svg?style=social&label=Star" />',
-        href: "https://github.com/saas-js/saas-ui",
+        href: "https://github.com/Volare-Solutions/",
         action: false,
       },
     },
